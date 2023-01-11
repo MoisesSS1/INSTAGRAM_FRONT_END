9830cc4535ad9a1db73793df6772891997b6974a
Patt0lino <investmentmoises@gmail.com>
2023-01-11 00:55:46 -0300

    finalizando cadastro do usuario no banco

--- src/pages/CreateUser/CreateUser.jsx
+++ src/pages/CreateUser/CreateUser.jsx
@@ -1,4 +1,7 @@
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
+
+import api from '../../utils/api'
 
 //css
 import Style from './CreateUser.module.css'
@@ -6,34 +9,44 @@ import Style from './CreateUser.module.css'
 //imagens
 import instagramLogin from '../../assets/instagramLogin.svg'
 
-
-
 const CreateUser = () => {
+
+  const [user, setUser] = useState({})
+
+  function handleOnChange(e){
+    setUser({...user,[e.target.name]:e.target.value})
+  }
+
+  async function  handleSumit(e) {
+    e.preventDefault()
+
+    api.post('user/create', user)
+    
+  }
+
   return (
     <div className={Style.container_home}>
           <div className={Style.options}>
             <div className={Style.form_login}>
 
-              <form >
+              <form onSubmit={(e)=>handleSumit(e)} >
                   <img src={instagramLogin} alt="instagram" />
                     <p>Inscreva-se para ver fotos e vídeos de seus amigos.</p>
 
                     <label htmlFor="name">
-                    <input type="text" name="name" id="name" placeholder='Digite seu nome' required/>
+                    <input onChange={(e)=>handleOnChange(e)} type="text" name="name" id="name" placeholder='Digite seu nome' required/>
                   </label>
 
                   <label htmlFor="email">
-                    <input type="email" name="email" id="email" placeholder='Digite se e-mail' required/>
+                    <input onChange={(e)=>handleOnChange(e)} type="email" name="email" id="email" placeholder='Digite se e-mail' required/>
                   </label>
                   <label htmlFor="password">
-                    <input type="password" name="password" id="password" placeholder='Digite sua senha' required/>
+                    <input onChange={(e)=>handleOnChange(e)} type="password" name="password" id="password" placeholder='Digite sua senha' required/>
                   </label>
 
                   <label htmlFor="phone">
-                    <input type="phone" name="phone" id="phone" placeholder='Digite seu telefone' required/>
+                    <input onChange={(e)=>handleOnChange(e)} type="phone" name="phone" id="phone" placeholder='Digite seu telefone' required/>
                   </label>
-
-
 
                   <label htmlFor="submit">
                     <input type="submit" value="Criar conta" /> 
@@ -42,9 +55,7 @@ const CreateUser = () => {
             </div>
 
             <div className={Style.option_createAccount}>
-
               <p>Já possui conta? <span><Link to='/'>Entrar</Link></span></p>
-
             </div>
 
         </div>
