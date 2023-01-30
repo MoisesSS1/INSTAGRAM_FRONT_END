1835d4cdb82c437b64535ec28f99f06ac8011bf5
Patt0lino <investmentmoises@gmail.com>
2023-01-30 01:55:53 -0300

    configs test

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 app = express()
-    app.use(express.static('reactFolderName/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'reactFolderName', 'build', 'index.html')));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
